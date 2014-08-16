8930da7e26357727d102ef25d070bb9364a3c344
braden <braden@jabblabs.com>
2014-08-16 00:17:18 -0500

    init version

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+//custom.js
+function changeContent(){
+	var newContent = '<h1>Simple and elegant.<small>==App Name== – an easy to use event finder <br class="visible-on-desktop">designed to help you find something to do.</small></h1><div class="button-group"><a class="large primary button contact-toggle " href="#">Download now</a><a class="large hollow button " href="#content" data-smoothscroll>Learn more</a></div>';
+	document.getElementById('mainSection').innerHTML = newContent;
+}
+
+                           
